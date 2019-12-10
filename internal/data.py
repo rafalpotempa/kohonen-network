c5575beb60e50e72804e8c7cfaf2b294bef08a26
Rafal Potempa <rafal.potem@gmail.com>
2019-12-10 20:03:33 +0100

    Clean data generation

--- internal/data.py
+++ internal/data.py
@@ -1,7 +1,4 @@
 from random import seed, gauss
-import numpy as np
-from matplotlib import pyplot as plt
-from matplotlib.animation import FuncAnimation
 
 seed(0)
 
@@ -9,31 +6,8 @@ def GenerateData(points, sigma=0.2):
 	data = []
 	for point in points:
 		for i in range(100):
-			data.append([gauss(point[0], sigma), gauss(point[1], sigma)])
+			data.append({
+				'x': gauss(point[0], sigma), 
+				'y': gauss(point[1], sigma)})
 	return data
-
-foci = [
-	[2,2], 
-	[2,4], 
-	[4,2]
-]
-
-plt.style.use('seaborn-pastel')
-
-fig = plt.figure()
-ax = plt.axes(xlim=(0, 10), ylim=(-2, 10))
-plt.scatter(*zip(*GenerateData(foci)))
-line, = ax.plot([], [], lw=3)
-
-def init():
-    line.set_data([], [])
-    return line,
-def animate(i):
-    x = np.linspace(0, 4, 1000)
-    y = np.sin(2 * np.pi * (x - 0.01 * i))
-    line.set_data(x, y)
-    return line,
-
-anim = FuncAnimation(fig, animate, init_func=init, frames=200, interval=20, blit=True)
-
-plt.show()							   
+							   
