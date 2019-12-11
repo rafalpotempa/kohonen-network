ea272cf4b06732934bd4ca458775542e29f924f2
Rafał Potempa <33723064+rafalpotempa@users.noreply.github.com>
2019-12-11 01:02:37 +0100

    Add plotting

--- internal/plot.py
+++ internal/plot.py
@@ -7,28 +7,22 @@ class Plot:
 		_data = {
 			'x': [point['x'] for point in data],
 			'y': [point['y'] for point in data]}
-
 		plt.scatter('x', 'y', data=_data)
 	def Show(self):
 		plt.show()
-	
 
-if __name__ == "__main__":
-	plt.style.use('seaborn-pastel')
+	# scat, = plt.scatter([], [])
+	#
+	# def Animate(self, stream): # not working yet
+	# 	pass
+	# 	fig = plt.figure()
+	# 	def init():
+	# 		self.scat.set_data([], [])
+	# 		return self.scat,
+	# 	def animate(self, i, stream):
+	# 		x = [point['x'] for point in stream['neurons']]
+	# 		y = [point['y'] for point in stream['neurons']]
+	# 		self.scat.set_data(x, y)
+	# 		return self.scat,
+	# 	anim = FuncAnimation(fig, animate, init_func=init,frames=100, blit=True, fargs=(stream))
 
-	fig= plt.figure()
-	ax= plt.axes(xlim=(0, 10), ylim=(-2, 10))
-	line,= ax.plot([], [], lw=3)
-
-	def init():
-		line.set_data([], [])
-		return line,
-	def animate(i):
-		x= np.linspace(0, 4, 1000)
-		y= np.sin(2 * np.pi * (x - 0.01 * i))
-		line.set_data(x, y)
-		return line,
-
-	anim= FuncAnimation(fig, animate, init_func=init, frames=200, interval=20, blit=True)
-
-	plt.show()
